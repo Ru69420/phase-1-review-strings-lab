2724cd4c41d47fd355141c63d8a4f1997c67a16c
RuthTadesse <tib6297@gmail.com>
2022-02-21 11:07:43 -0500

    commit tree 40b414c4042aaa8e2df85d926fee66e6351b4a6b

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, GRACE HOPPER !';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, G!';
-
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
